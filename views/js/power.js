c5edb1b133ca2471f591a82e9bf01a5851bf545c
vincenthure <vincenthure@mac.com>
2019-01-31 21:11:18 +0100

    31/01/2019

--- views/js/power.js
+++ views/js/power.js
@@ -1,11 +1,11 @@
 
 
-document.getElementById("eteindre").addEventListener(  "click", function()    { commande("shutdown") })
-document.getElementById("redemarrer").addEventListener("click", function()    { commande("reboot")   })
-document.getElementById("reload").addEventListener("click", function()        { remote("service")    })
-document.getElementById("remote_off").addEventListener("click", function()    { remote("shutdown")   })
-document.getElementById("remote_reboot").addEventListener("click", function() { remote("reboot")     })
-document.getElementById("desktop").addEventListener("click", function()       { remote("desktop")   })
+document.getElementById("pilote_off").addEventListener(  "click", function()     { commande("shutdown") })
+document.getElementById("pilote_reboot").addEventListener("click", function()    { commande("reboot")   })
+document.getElementById("pilote_reload").addEventListener("click", function()    { commande("service")  })
+document.getElementById("remote_off").addEventListener("click", function()       { remote("shutdown")   })
+document.getElementById("remote_reboot").addEventListener("click", function()    { remote("reboot")     })
+document.getElementById("remote_reload").addEventListener("click", function()    { remote("service")    })
 
 function remote(str)
     {
